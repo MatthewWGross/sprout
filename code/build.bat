2d4cad9574be299e2a366141cc0c2460eb407232
Matt <matthew.w.gross@gmail.com>
2020-01-28 15:41:42 -0800

    fixing library build args

--- code/build.bat
+++ code/build.bat
@@ -1,8 +1,9 @@
 @echo off
 set INCLUDE=..\code\external\include;%INCLUDE%
+set LIB=..\code\external\lib;%LIB%
 set defines=-DGLEW_STATIC -D_CONSOLE
 set commonCompilerFlags=-MD -nologo -Od -Oi -Gm- -GR- -EHa-  -W4 -wd4201 -wd4100 -wd4189 %defines% -Z7 -FC -Fmsprout.map
-set commonLinkerFlags= -opt:ref user32.lib Gdi32.lib shell32.lib oleaut32.lib GLEW_1130_RELEASE.lib glfw3_RELEASE.lib opengl32.lib glu32.lib
+set commonLinkerFlags= -opt:ref user32.lib Gdi32.lib shell32.lib oleaut32.lib GLEW_1130_DEBUG.lib glfw3_DEBUG.lib opengl32.lib glu32.lib
 set cppfiles=..\code\src\application.cpp ..\code\src\main.cpp ..\code\src\shader.cpp ..\code\src\window.cpp
 
 IF NOT EXIST ..\build mkdir ..\build
