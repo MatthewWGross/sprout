1a666b319d87d244b02cf91822071484be9f5f5d
Matt <matthew.w.gross@gmail.com>
2020-04-27 06:50:48 -0700

    Added model loading and removed extraneous color buffer

--- code/build.bat
+++ code/build.bat
@@ -4,7 +4,7 @@ set LIB=..\code\external\lib;%LIB%
 set defines=-DGLEW_STATIC -D_CONSOLE
 set commonCompilerFlags=-MD -nologo -Od -Oi -Gm- -GR- -EHa-  -W4 -wd4201 -wd4100 -wd4189 %defines% -Z7 -FC -Fmsprout.map
 set commonLinkerFlags= -opt:ref user32.lib Gdi32.lib shell32.lib oleaut32.lib GLEW_1130_DEBUG.lib glfw3_DEBUG.lib opengl32.lib glu32.lib
-set cppfiles=..\code\src\winplatform.cpp ..\code\src\main.cpp ..\code\src\shader.cpp
+set cppfiles=..\code\src\winplatform.cpp ..\code\src\main.cpp ..\code\src\shader.cpp ..\code\src\objloader.cpp
 
 IF EXIST ..\build rm -rf ..\build
 mkdir ..\build
--- code/src/winplatform.cpp
+++ code/src/winplatform.cpp
@@ -3,7 +3,7 @@
 // Include standard headers
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <vector>
 // Include GLEW
 #include <glew.h>
 
@@ -19,129 +19,16 @@ using namespace glm;
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
+#include "objloader.hpp"
+
 // Dumping ground for temporary globals
 namespace
 {
-// Our vertices. Three consecutive floats give a 3D vertex; Three consecutive vertices give a triangle.
-// A cube has 6 faces with 2 triangles each, so this makes 6*2=12 triangles, and 12*3 vertices
-static const GLfloat g_vertex_buffer_data[] = {
-    -1.0f, -1.0f, -1.0f, // triangle 1 : begin
-    -1.0f, -1.0f, 1.0f,
-    -1.0f, 1.0f, 1.0f, // triangle 1 : end
-    1.0f, 1.0f, -1.0f, // triangle 2 : begin
-    -1.0f, -1.0f, -1.0f,
-    -1.0f, 1.0f, -1.0f, // triangle 2 : end
-    1.0f, -1.0f, 1.0f,
-    -1.0f, -1.0f, -1.0f,
-    1.0f, -1.0f, -1.0f,
-    1.0f, 1.0f, -1.0f,
-    1.0f, -1.0f, -1.0f,
-    -1.0f, -1.0f, -1.0f,
-    -1.0f, -1.0f, -1.0f,
-    -1.0f, 1.0f, 1.0f,
-    -1.0f, 1.0f, -1.0f,
-    1.0f, -1.0f, 1.0f,
-    -1.0f, -1.0f, 1.0f,
-    -1.0f, -1.0f, -1.0f,
-    -1.0f, 1.0f, 1.0f,
-    -1.0f, -1.0f, 1.0f,
-    1.0f, -1.0f, 1.0f,
-    1.0f, 1.0f, 1.0f,
-    1.0f, -1.0f, -1.0f,
-    1.0f, 1.0f, -1.0f,
-    1.0f, -1.0f, -1.0f,
-    1.0f, 1.0f, 1.0f,
-    1.0f, -1.0f, 1.0f,
-    1.0f, 1.0f, 1.0f,
-    1.0f, 1.0f, -1.0f,
-    -1.0f, 1.0f, -1.0f,
-    1.0f, 1.0f, 1.0f,
-    -1.0f, 1.0f, -1.0f,
-    -1.0f, 1.0f, 1.0f,
-    1.0f, 1.0f, 1.0f,
-    -1.0f, 1.0f, 1.0f,
-    1.0f, -1.0f, 1.0f};
-
-// One color for each vertex. They were generated randomly.
-static const GLfloat g_color_buffer_data[] = {
-    0.583f, 0.771f, 0.014f,
-    0.609f, 0.115f, 0.436f,
-    0.327f, 0.483f, 0.844f,
-    0.822f, 0.569f, 0.201f,
-    0.435f, 0.602f, 0.223f,
-    0.310f, 0.747f, 0.185f,
-    0.597f, 0.770f, 0.761f,
-    0.559f, 0.436f, 0.730f,
-    0.359f, 0.583f, 0.152f,
-    0.483f, 0.596f, 0.789f,
-    0.559f, 0.861f, 0.639f,
-    0.195f, 0.548f, 0.859f,
-    0.014f, 0.184f, 0.576f,
-    0.771f, 0.328f, 0.970f,
-    0.406f, 0.615f, 0.116f,
-    0.676f, 0.977f, 0.133f,
-    0.971f, 0.572f, 0.833f,
-    0.140f, 0.616f, 0.489f,
-    0.997f, 0.513f, 0.064f,
-    0.945f, 0.719f, 0.592f,
-    0.543f, 0.021f, 0.978f,
-    0.279f, 0.317f, 0.505f,
-    0.167f, 0.620f, 0.077f,
-    0.347f, 0.857f, 0.137f,
-    0.055f, 0.953f, 0.042f,
-    0.714f, 0.505f, 0.345f,
-    0.783f, 0.290f, 0.734f,
-    0.722f, 0.645f, 0.174f,
-    0.302f, 0.455f, 0.848f,
-    0.225f, 0.587f, 0.040f,
-    0.517f, 0.713f, 0.338f,
-    0.053f, 0.959f, 0.120f,
-    0.393f, 0.621f, 0.362f,
-    0.673f, 0.211f, 0.457f,
-    0.820f, 0.883f, 0.371f,
-    0.982f, 0.099f, 0.879f};
-
-// UV's for each vertex
-static const GLfloat g_uv_data[] = {
-    0.583f, 1.0f,
-    0.609f, 0.436f,
-    0.327f, 0.0f,
-    0.822f, 0.201f,
-    0.435f, 0.223f,
-    0.0f, 0.185f,
-    1.0f, 0.761f,
-    0.559f, 0.730f,
-    0.359f, 0.152f,
-    0.483f, 0.789f,
-    0.559f, 0.639f,
-    0.195f, 0.859f,
-    0.014f, 0.576f,
-    0.771f, 0.970f,
-    0.406f, 0.116f,
-    0.676f, 0.133f,
-    0.971f, 0.833f,
-    0.140f, 0.489f,
-    0.997f, 0.064f,
-    0.945f, 0.592f,
-    0.543f, 0.978f,
-    0.279f, 0.505f,
-    0.167f, 0.077f,
-    0.347f, 0.137f,
-    0.055f, 0.042f,
-    0.714f, 0.345f,
-    0.783f, 0.734f,
-    0.722f, 0.174f,
-    0.302f, 0.848f,
-    0.225f, 0.040f,
-    0.517f, 0.338f,
-    0.053f, 0.120f,
-    0.393f, 0.362f,
-    0.673f, 0.457f,
-    0.820f, 0.371f,
-    0.982f, 0.879f};
+std::vector<glm::vec3> vertices;
+std::vector<glm::vec2> uvs;
+std::vector<glm::vec3> normals; // Won't be used at the moment.
 
 // This will identify our vertex buffer
-GLuint colorbuffer;
 GLuint uvbuffer;
 GLuint vertexbuffer;
 GLuint VertexArrayID;
@@ -200,25 +87,6 @@ void InitPlatformAndWindow()
     // Get a handle for our "MVP" uniform
     MatrixID = glGetUniformLocation(programID, "MVP");
 
-    // Bind our VBO Data
-    glGenVertexArrays(1, &VertexArrayID);
-    glBindVertexArray(VertexArrayID);
-
-    // Generate 1 buffer, put the resulting identifier in vertexbuffer
-    glGenBuffers(1, &vertexbuffer);
-    // The following commands will talk about our 'vertexbuffer' buffer
-    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-    // Give our vertices to OpenGL.
-    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
-
-    glGenBuffers(1, &colorbuffer);
-    glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
-
-    glGenBuffers(1, &uvbuffer);
-    glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(g_uv_data), g_uv_data, GL_STATIC_DRAW);
-
     // Enable depth test
     glEnable(GL_DEPTH_TEST);
     // Accept fragment if it closer to the camera than the former one
@@ -226,9 +94,9 @@ void InitPlatformAndWindow()
 
     printf("Loading Image\n");
     glGenTextures(1, &textureID);
-    glBindTexture(GL_TEXTURE_2D, textureID); 
-     // set the texture wrapping parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    // set the texture wrapping parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT); // set texture wrapping to GL_REPEAT (default wrapping method)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     // set texture filtering parameters
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
@@ -241,16 +109,28 @@ void InitPlatformAndWindow()
     {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
-        printf("Image Loaded successfully!, width %i, height %i", width, height);
+        printf("Image Loaded successfully!, width %i, height %i\n", width, height);
     }
     else
     {
-        printf("Failed to load texture");
+        printf("Failed to load texture\n");
     }
     stbi_image_free(data);
 
+    // Read our .obj file
+    bool res = loadOBJ("./data/models/cube.obj", vertices, uvs, normals);
 
+    // Bind our VBO Data
+    glGenVertexArrays(1, &VertexArrayID);
+    glBindVertexArray(VertexArrayID);
 
+    glGenBuffers(1, &vertexbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec3), &vertices[0], GL_STATIC_DRAW);
+
+    glGenBuffers(1, &uvbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
+    glBufferData(GL_ARRAY_BUFFER, uvs.size() * sizeof(glm::vec2), &uvs[0], GL_STATIC_DRAW);
 }
 
 bool UpdateInput()
@@ -288,7 +168,6 @@ void UpdateRender()
     // Clear the screen. It's not mentioned before Tutorial 02, but it can cause flickering, so it's there nonetheless.
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, textureID);
 
@@ -310,21 +189,9 @@ void UpdateRender()
 
     // 2nd attribute buffer : colors
     glEnableVertexAttribArray(1);
-    glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
-    glVertexAttribPointer(
-        1,        // attribute. No particular reason for 1, but must match the layout in the shader.
-        3,        // size
-        GL_FLOAT, // type
-        GL_FALSE, // normalized?
-        0,        // stride
-        (void *)0 // array buffer offset
-    );
-
-    // 2nd attribute buffer : colors
-    glEnableVertexAttribArray(2);
     glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
     glVertexAttribPointer(
-        2,        // attribute. No particular reason for 1, but must match the layout in the shader.
+        1,        // attribute. No particular reason for 1, but must match the layout in the shader.
         2,        // size
         GL_FLOAT, // type
         GL_FALSE, // normalized?
@@ -333,10 +200,9 @@ void UpdateRender()
     );
 
     // Draw the triangle !
-    glDrawArrays(GL_TRIANGLES, 0, 12 * 3); // 12*3 indices starting at 0 -> 12 triangles -> 6 squares
+    glDrawArrays(GL_TRIANGLES, 0, static_cast<GLsizei>(vertices.size()));
     glDisableVertexAttribArray(0);
     glDisableVertexAttribArray(1);
-    glDisableVertexAttribArray(2);
 
     // Swap buffers
     glfwSwapBuffers(window);
@@ -346,7 +212,6 @@ void Shutdown()
 {
     // Cleanup VBO
     glDeleteBuffers(1, &vertexbuffer);
-    glDeleteBuffers(1, &colorbuffer);
     glDeleteBuffers(1, &uvbuffer);
     glDeleteVertexArrays(1, &VertexArrayID);
     glDeleteProgram(programID);
